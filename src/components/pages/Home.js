082a9a53271dedfd9e77908fbc8ef05249af2173
Yousof Wakili <ywakili18@gmail.com>
2021-12-23 12:31:29 -0800

    Landing page done, responsive, styled 🙌

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 const Home = () => {
+  const history = useNavigate()
+  const onClick = () => {
+    history('/search')
+  }
   return (
-    <div>
-      <h1>home page bruh</h1>
+    <div className="text-sm sm:text-2xl md:text-4xl">
+      <h1 className="threePtTitle text-center mt-2">
+        3pointshot{' '}
+        <img src="https://i.giphy.com/media/xT9IgCfqoMQEWOUef6/giphy.webp" />
+      </h1>
+      <div className="home">
+        <p>search for nba player stats</p>
+        <button
+          onClick={onClick}
+          className="
+            enterBtn border-2 rounded-lg p-1 
+          text-orange-300 
+          hover:text-black hover:bg-orange-400 hover:border-black ml-4"
+        >
+          kobe!
+        </button>
+      </div>
     </div>
   )
 }
