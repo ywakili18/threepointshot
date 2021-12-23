72eed0fa307bf0b55e057c118445bf6791c9cda1
Yousof Wakili <ywakili18@gmail.com>
2021-12-23 11:55:09 -0800

    Navbar set up, responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { GiBasketballBasket } from 'react-icons/gi'
+import { FaGithub } from 'react-icons/fa'
 const NavBar = () => {
   return (
-    <nav className="p-2 text-2xl bg-orange-400  flex justify-between">
+    <nav className="p-4 text-md sm:text-2xl bg-orange-400 flex justify-around rounded-md border-2 border-black">
       <NavLink to="/" className="nav">
-        <GiBasketballBasket />
+        <GiBasketballBasket className="text-xl sm:text-3xl" />
       </NavLink>
       <NavLink to="/search" className="nav">
         Players
       </NavLink>
       <NavLink to="/about" className="nav">
-        About
+        about
       </NavLink>
-      <NavLink to="/about" className="nav">
-        Github Repo{' '}
-      </NavLink>
+      <a
+        href="https://github.com/ywakili18/threepointshot"
+        className="nav"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaGithub className="text-xl sm:text-3xl" />
+      </a>
     </nav>
   )
 }
