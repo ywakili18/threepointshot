490f97eade638d735130db89917f7d5d7190552e
Yousof Wakili <ywakili18@gmail.com>
2021-12-22 21:57:03 -0800

    tested basic api call with useeffect, looks good 🙌

--- src/components/QueryApi.js
+++ src/components/QueryApi.js
@@ -1,6 +1,16 @@
 import React from 'react'
-
+import { useEffect } from 'react'
+import axios from 'axios'
 const QueryApi = ({ player }) => {
+  useEffect(() => {
+    const getResults = () => {
+      axios
+        .get(`https://www.balldontlie.io/api/v1/players/?search=${player}`)
+        .then((res) => console.log(res.data))
+    }
+    getResults()
+  }, [])
+
   return (
     <div>
       <h1>This is QueryApi component.</h1>
